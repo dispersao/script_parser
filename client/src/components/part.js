50a48a9235379b527f34b7b0947ea4c44532d917
mairasala <mairasala@gmail.com>
2018-11-04 20:30:48 +0100

    print observation with different color

--- client/src/components/part.js
+++ client/src/components/part.js
@@ -6,17 +6,20 @@ class Part extends Component {
     let partHeader = "";
     if(this.props.type === 'dialogue'){
       partHeader += this.props.characters[0].name;
+      if(this.props.extra){
+        partHeader += ` ${this.props.extra}`;
+      }
     }
-    if(this.props.extra){
-      partHeader += ` ${this.props.extra}`;
+    let classes = "partContent";
+    if(this.props.type === 'observation'){
+      classes += " light";
     }
-
     return (
       <div className="partContainer">
         {partHeader && partHeader.length > 0 &&
           <h4 className="partHeader">{partHeader}</h4>
         }
-        <div className="partContent">{this.props.content}</div>
+        <div className={classes}>{this.props.content}</div>
       </div>
     )
   }
