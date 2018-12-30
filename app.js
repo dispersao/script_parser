48751d0631225b4711c26ddc3c3a021a75142c9b
mairasala <mairasala@gmail.com>
2018-12-30 12:15:30 -0200

    serve api in /api route

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
 
-var indexRouter = require('./routes/index');
+// var indexRouter = require('./routes/index2');
+var apiRouter = require('./routes/index');
 
 var app = express();
 
@@ -23,7 +24,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
-app.use('/', indexRouter);
+app.use('/api', apiRouter);
+app.use(express.static('client/build'));
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
